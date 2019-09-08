ead94531cf090dae3c3d8088f52bc5e69322e0b2
Aia Rupsom <aiale88@gmail.com>
2019-09-08 12:14:17 +0200

    create function onDragEnd to persist new order in a column

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -4,12 +4,35 @@ import Column from '../Column/index';
 import { DragDropContext } from 'react-beautiful-dnd';
 
 const App = () => {
-  const [studentsList, setStudent] = useState(students);
-  const [columnsList, setColumn] = useState(columns);
+  const [studentsList, setStudents] = useState(students);
+  const [columnsList, setColumns] = useState(columns);
   const [columnsOrder, setOrder] = useState(columnOrder);
 
   const onDragEnd = result => {
-    // TODO: reorder our column
+    const { destination, source, draggableId } = result;
+    if (
+      !destination ||
+      (destination.droppableId === source.droppableId &&
+        destination.index === source.index)
+    )
+      return;
+
+    const column = columnsList.find(column => column.id === source.droppableId);
+    const newStudentsIds = Array.from(column.studentsIds);
+    newStudentsIds.splice(source.index, 1);
+    newStudentsIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      studentsIds: newStudentsIds
+    };
+
+    const newColumnsList = [...columnsList];
+    newColumnsList[
+      columnsList.map(column => column.id).indexOf(newColumn.id)
+    ] = newColumn;
+
+    setColumns(newColumnsList);
   };
 
   return (
